b12bb73a1f59e3f76f68f179be685d3213adecf0
Snehlata12389 <snehlata12389@gmail.com>
2023-04-10 17:13:15 +0530

    Changes done as per comments

--- SqlFilmstesting.sql
+++ SqlFilmstesting.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dbo.Actor(
-	ActorID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	FirstName nvarchar(100) NOT NULL,
-	FamilyName nvarchar(100) NULL,
-	FullName  AS (FirstName+isnull(' '+FamilyName,'')),
-	DoB datetime NULL,
-	DoD datetime NULL,
-	Gender nvarchar(20) NULL)
+	actorID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	firstName nvarchar(100) NOT NULL,
+	familyName nvarchar(100) NULL,
+	fullName  AS (FirstName+isnull(' '+FamilyName,'')),
+	foB datetime NULL,
+	foD datetime NULL,
+	gender nvarchar(20) NULL)
