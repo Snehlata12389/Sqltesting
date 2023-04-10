bc0d1a7afe946a9bd4292b90e830d9b0e694c9e8
sudharya0506 <75392551+sudharya0506@users.noreply.github.com>
2023-04-10 17:17:56 +0530

    Revert "Changes added to the files"

--- SqlFilmstesting.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE dbo.Actor(
-	actorID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	firstName nvarchar(100) NOT NULL,
-	familyName nvarchar(100) NULL,
-	fullName  AS (FirstName+isnull(' '+FamilyName,'')),
-	foB datetime NULL,
-	foD datetime NULL,
-	gender nvarchar(20) NULL)
